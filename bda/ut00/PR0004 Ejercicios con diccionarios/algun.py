d153902aa417ceb95f90ffa6a51e41c2f82ec629
GuillenteN <gdiez05@gmail.com>
2025-10-07 09:45:13 +0200

    Commit 3

---
+++ bda/ut00/PR0004 Ejercicios con diccionarios/algun.py
@@ -0,0 +1,9 @@
+#Escribe un programa que tome una cadena y use un diccionario para contar cuantas veces aparece cada caracter
+
+str = "Especialización en Inteligencia Artificial y Big Data"
+res = {}
+for char in str.lower():
+    if char in res:
+        res[char] += 1
+    else:
+        res[char] = 1
